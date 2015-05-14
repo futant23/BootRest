51def25992cd4ff39120e3837c547135d4fe6671
b1050502 <b1050502@A5470394.ne.nos.boeing.com>
2015-05-14 12:58:20 -0400

    Initial

--- src/main/java/rtsw/toolsteam/bootrest/service/WeatherService.java
+++ src/main/java/rtsw/toolsteam/bootrest/service/WeatherService.java
@@ -5,38 +5,67 @@
  */
 package rtsw.toolsteam.bootrest.service;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import rtsw.toolsteam.bootrest.domain.Current;
 
 /**
  *
- * @author 
+ * @author
  */
 @Service
 public class WeatherService {
-    
-    private final static Logger log =LoggerFactory.getLogger(WeatherService.class);
-    
-    
-    
-    public WeatherService() {}
-    
+
+    private final static Logger log = LoggerFactory.getLogger(WeatherService.class);
+
+    @Value(value = "${proxy.host}")
+    private String proxyHost;
+
+    @Value(value = "${proxy.port}")
+    private Integer proxyPort;
+
+    @Value(value = "${proxy.required}")
+    private Boolean proxyRequired;
+
+    public WeatherService() {
+    }
+
     public Current getCurrent(String location) {
         log.debug("getCurrent()");
-        
-        final String URL ="http://api.openweathermap.org/data/2.5/weather?q={location}&mode=xml";       
-        RestTemplate restTemplate =new RestTemplate();
-        Map<String, String> map =new HashMap<String, String>();
+
+        final String URL = "http://api.openweathermap.org/data/2.5/weather?q={location}&mode=xml";
+
+        RestTemplate restTemplate = getRestTemplate();
+        Map<String, String> map = new HashMap<String, String>();
         map.put("location", location);
-        
-        Current current =restTemplate.getForObject(URL, Current.class, map);
-        log.info("Located Current weather: "+ current);
-        
+
+        Current current = restTemplate.getForObject(URL, Current.class, map);
+        log.info("Located Current weather: " + current);
+
         return current;
     }
+
+    private RestTemplate getRestTemplate() {
+
+        
+        if (proxyRequired) {
+            SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+            Proxy proxy = new Proxy(Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+            requestFactory.setProxy(proxy);
+
+            return new RestTemplate(requestFactory);
+        }
+        else 
+            return new RestTemplate();
+    }
+
 }
